0df08eec56e9aa1ca60fe07acda95f7b1e622c2b
zaytsevp <paz1506@gmail.com>
2020-07-01 22:10:17 +0300

    init commit

---
+++ baeldung-grpc-example/src/main/java/com/zaytsevp/baeldunggrpcexample/BaeldungGrpcExampleApplication.java
@@ -0,0 +1,13 @@
+package com.zaytsevp.baeldunggrpcexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeldungGrpcExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaeldungGrpcExampleApplication.class, args);
+    }
+
+}
---
+++ baeldung-grpc-example/src/test/java/com/zaytsevp/baeldunggrpcexample/BaeldungGrpcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaytsevp.baeldunggrpcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeldungGrpcExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
